75f2c6b45b9c02ca9da2af46e949bdca4a52a6e9
Maxime Rosay <47741889+Miaouf@users.noreply.github.com>
2021-01-05 15:41:23 +0100

    Create wireshark_png_extract.sh

---
+++ scripts/wireshark_png_extract.sh
@@ -0,0 +1,12 @@
+#  Given a TCP capture, this script allows you to get images from it
+# run it like ./wireshark_png_extract.sh [capture file]
+# Thanks to Abdelouahab Laaroussi for his contribution
+
+
+infile=$1
+
+for stream in $(tshark -nlr $infile -Y tcp.flags.syn==1 -T fields -e tcp.stream | sort -n | uniq | sed 's/\r//')
+do
+    echo "Processing stream $stream: img_${stream}.png"
+    tshark -nlr $infile -qz "follow,tcp,raw,$stream" | tail -n +7 | sed 's/^\s\+//g' | xxd -r -p > img_${stream}.png
+done
